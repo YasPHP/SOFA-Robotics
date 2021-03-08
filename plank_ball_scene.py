cb25162be0c822131df76c87240a6045c56f36dd
Yasmeen Brain <47681976+YasPHP@users.noreply.github.com>
2021-03-08 16:56:41 -0500

    basic Mechanical Model

--- plank_ball_scene.py
+++ plank_ball_scene.py
@@ -10,4 +10,15 @@ def createScene(rootNode):
   
   cube = rootNode.createChild("Cube")
   
-
+  # Mechanical Model
+  
+  totalMass = 5.0
+  volume = 5.0
+  inertiaMatrix = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+  
+  cube.createObject('MechanicalObject', name="DOF", 
+                    template="Rigid", translation=[0.0, 0.0, 0.0], 
+                    rotation = [0.0, 0.0, 0.0])
+  
+  cube.createObject('UniformMass', name="Mass", 
+                    mass=[totalMass, volume, inertiaMatrix[:]])
