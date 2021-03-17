a98049b948d6e3d0c6aed888001bdc892892ec46
Yasmeen Brain <47681976+YasPHP@users.noreply.github.com>
2021-03-17 15:56:49 -0400

    mechanical model components

--- scene_template.py
+++ scene_template.py
@@ -41,19 +41,61 @@ def createScene(rootNode):
          uniformScale=0.0)
     
     
-   # creating children (ie. a cube) for the root node
+  # creating children (ie. a cube) for the root node
   
-    cube = rootNode.createChild("Cube")
+  cube = rootNode.createChild("Cube")
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  # Mechanical Model
+  
+  totalMass = 0.0
+  volume = 0.0
+  inertiaMatrix = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+  
+  # create an Object
+  cube.createObject('ObjectName', name="name", 
+                    template="Rigid", translation=[0.0, 0.0, 0.0], 
+                    rotation = [0.0, 0.0, 0.0])
+  
+  # create a Uniform Mass Object
+  cube.createObject('UniformMass', name="Mass", 
+                    mass=[totalMass, volume, inertiaMatrix[:]])
+
+  # Time Integration Scheme
+  cube.createObject('EulerImplicit', name="")
+  
+  # Solving Method
+  cube.createObject('CGLinearSolver', name="")
+  
+  # create a visual object of child object (ie. cube)
+  visual = cube.createChild("Cube Visual")
+  
+  # Visual Ogl Model
+  visual.createObject('OglModel', name="Visual",
+                      fileMesh="mesh/name.ob", 
+                      colour=[0.0,0.0,0.0],
+                      scale=0.0)
+  
+  # Rigid Mapping Object
+  visual.createObject('RigidMapping')
+  
+  # Collision Model for the Cube
+  
+  collision = cube.createChild("Cube Collision Model")
+  
+  # Creating Collision Objects
+  collision.createObject('MeshObjLoader', name="Loader",
+                        filename="mesh/smCube27.obj",
+                        triangulate="true", scale=20.0)
+  
+  collision.createObject('Mesh', src="@loader")
+  
+  collision.createObject('MechanicalObject')
+  
+  collision.createObject('Triangle')
+  collision.createObject('Line')
+  collision.createObject('Point')
+  
+  collision.createObject('RigidMapping')
     
     
   
