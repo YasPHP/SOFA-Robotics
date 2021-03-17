1ccc86af5738323f592ff57bfe62e5514ea4e1df
Yasmeen Brain <47681976+YasPHP@users.noreply.github.com>
2021-03-17 15:35:16 -0400

    default params

--- scene_template.py
+++ scene_template.py
@@ -3,39 +3,40 @@ from stlib.scene import MainHeader, ContactHeader
 from stlib.visuals import ShowGrid
 from stlib.physics import Floor
 from stlib.physics import Cube
+from stlib.solver import DefaultSolver
 
 def createScene(rootNode):
 
   # main header (rootNode + gravity)
-  MainHeader(rootNode, gravity=[0.0,-981.0,0.0])
+  MainHeader(rootNode, gravity=[])
   
   # contact header
-  ContactHeader(rootNode, alarmDistance=8,
-               contactDistance=5)
+  ContactHeader(rootNode, alarmDistance=0,
+               contactDistance=)
 
   # shows the grid outline
   ShowGrid(rootNode)
   
   # base floor
   Floor(rootNode,
-       translation=[0.0,-180.0,0.0],
-       uniformScale=5.0,
+       translation=[],
+       uniformScale=0.0,
        isAStaticObject=True)
   
   # obstacle floor
   Floor(rootNode,
         name="Floor Obstacle",
-        translation=[0.0,-90.0,0.0],
-        color=[0.0,1.0,0.0],
-        uniformScale=0.8,
+        translation=[],
+        color=[],
+        uniformScale=0.0,
         isAStaticObject=True)
   
-  # creates 7 cubes
-  for i in range(7):
+  # creates cubes
+  for i in range(0):
     Cube(rootNode,
          name=,
          translation=[],
-         color=[c/10.0,c*0.7/10.0,0.9],
-         uniformScale=20.0)
+         color=[],
+         uniformScale=0.0)
   
   return rootNode
